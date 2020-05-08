164171836481667d5adc43939cd3d2d78a54b1b1
srikanth4c <srikanthbinf@gmail.com>
2020-05-09 01:08:30 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-GitHubCalendar('.calendar', "Ssrikanht4c", {responsive:!0})
+// GitHubCalendar(".calendar", "Ssrikanth4c", );
+    // or enable responsive functionality
+GitHubCalendar(".calendar", "Ssrikanth4c", { responsive: true,global_stats:true });
+
+const CalendarGraph = require("github-calendar-graph");
+
